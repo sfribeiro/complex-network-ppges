2e25b03699155f5fb5ad476b665dce3067364f44
serginhofribeiro@gmail.com <serginhofribeiro@gmail.com@8b1e764a-bc4a-88c5-8479-207cffb3e58c>
2014-09-03 01:57:42 +0000

    Iniciando o projeto de Ciência das Redes

---
+++ trunk/src/utils/DegreeMatrix.java
@@ -0,0 +1,36 @@
+package utils;
+public class DegreeMatrix {
+	public static int[][] calculate(int[][] matrix) {
+		int[][] m = new int[matrix.length][matrix.length];
+		int degree = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			degree = 0;
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (i != j) {
+					m[i][j] = 0;
+				}
+				degree += matrix[i][j];
+			}
+			m[i][i] = degree;
+		}
+
+		return m;
+	}
+	
+	public static double[][] calculateDouble(double[][] matrix) {
+		double[][] m = new double[matrix.length][matrix.length];
+		int degree = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			degree = 0;
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (i != j) {
+					m[i][j] = 0;
+				}
+				degree += matrix[i][j];
+			}
+			m[i][i] = degree;
+		}
+
+		return m;
+	}
+}
