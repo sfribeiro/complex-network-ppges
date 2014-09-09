3d4962d90188a4d253546b2750617f35d33d9548
serginhofribeiro@gmail.com <serginhofribeiro@gmail.com@8b1e764a-bc4a-88c5-8479-207cffb3e58c>
2014-09-09 00:56:29 +0000

    Criação do GraphViewer

---
+++ src/gstream/GraphViewer.java
@@ -0,0 +1,65 @@
+package gstream;
+
+import org.graphstream.graph.Node;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.implementations.MultiGraph;
+
+public class GraphViewer {
+	
+	private MultiGraph grafo;
+	
+	public GraphViewer() {
+		init();
+	}
+	
+	public void addNode(int i) {
+		Node node = this.grafo.getNode(i);
+		if (node == null) {
+			this.grafo.addNode(i+"");
+			this.grafo.getNode(i).addAttribute("ui.label", i+"");
+		}
+	}
+
+	public void addEdge(int i, int j) {
+		Edge edge = this.grafo.getEdge(i+"-"+j);
+		if (edge == null) {
+			this.grafo.addEdge(edgeName(i, j), i+"", j+"");
+		}
+		
+	}
+
+	public void highlightEdge(int i, int j, boolean set) {
+		Edge edge = this.grafo.getEdge(i+"-"+j);
+		if (edge != null) {
+			if (set) {
+				edge.addAttribute("ui.class", "influence");
+			} else {
+				edge.removeAttribute("ui.class");
+			}
+		} else {
+			System.err.print("Edge not found");
+		}
+	}
+
+	public void init() {
+		this.grafo = new MultiGraph("GraphViewer");
+		this.grafo.addAttribute("ui.stylesheet", "url('gstream/style.css')");
+	}
+	
+	public void display() {
+		this.grafo.display();		
+	}
+
+	public MultiGraph getGrafo() {
+		return grafo;
+	}
+
+	public void setGrafo(MultiGraph grafo) {
+		this.grafo = grafo;
+	}
+	
+	private String edgeName(int i, int j) {
+		return i+"-"+j;
+	}
+
+}
