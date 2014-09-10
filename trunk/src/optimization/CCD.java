775c90b87cd63e4174639dfa34d07498ca21eca7
jamissonfreitas@gmail.com <jamissonfreitas@gmail.com@8b1e764a-bc4a-88c5-8479-207cffb3e58c>
2014-09-10 14:28:02 +0000

    Versão inicial de otimização usando NSGAII para gerar redes que atendam duas metricas

---
+++ trunk/src/optimization/CCD.java
@@ -0,0 +1,119 @@
+package optimization;
+
+import org.graphstream.graph.implementations.MultiGraph;
+
+import gstream.GraphViewer;
+import metrics.ClusteringCoefficient;
+import metrics.Diameter;
+import jmetal.core.Problem;
+import jmetal.core.Solution;
+import jmetal.encodings.solutionType.BinarySolutionType;
+import jmetal.encodings.variable.Binary;
+import jmetal.util.JMException;
+
+public class CCD extends Problem{
+
+	private static final long serialVersionUID = 1L;
+	private ClusteringCoefficient CC = new ClusteringCoefficient();
+	private Diameter D = new Diameter();
+
+	public CCD(String solutionType, Integer numberOfVariables) {
+		numberOfVariables_ = 1;
+		numberOfObjectives_ = 2;
+		numberOfConstraints_ = 0;
+		problemName_ = "CCD";
+		length_ = new int[2];
+		length_[0]= (numberOfVariables * (numberOfVariables - 1))/2;
+		length_[1]= (numberOfVariables * (numberOfVariables - 1))/2;
+
+		upperLimit_ = new double[numberOfVariables_];
+		lowerLimit_ = new double[numberOfVariables_];
+
+		// Establishes upper and lower limits for the variables
+		for (int var = 0; var < numberOfVariables_; var++) {
+			lowerLimit_[var] = 0.0;
+			upperLimit_[var] = 1.0;
+		} // for
+
+		if (solutionType.compareTo("BinarySolution") == 0)
+			solutionType_ = new BinarySolutionType(this);
+		else {
+			System.out.println("Error: solution type " + solutionType
+					+ " invalid");
+			System.exit(-1);
+		}
+	} 
+	
+	public void evaluate(Solution solution) throws JMException {
+		Binary x = (Binary) solution.getDecisionVariables()[0];
+
+		double[] f = new double[numberOfObjectives_];
+		
+		double[][] m = getMatrix(x);
+		MultiGraph graph = getGraph(m);
+		
+		double f0 =  (double) CC.calculate(m, graph);
+		double f1 =  (double) D.calculate(m, graph);
+
+		if(f0 > 0)
+		f[0] = 1 / ( f0 );
+		else
+			f[0] = 1000;
+		
+		if(f1 > 0)
+		f[1] = 1 / ( f1 );
+		else
+			f[1] = 1000;
+
+		solution.setObjective(0, f[0]);
+		solution.setObjective(1, f[1]);
+		
+	}
+	
+	private double[][] getMatrix(Binary x) throws JMException
+	{
+		int size = 7;// (int) Math.sqrt(x.getNumberOfBits());
+		
+		double[][] resp = new double[size][size];
+		
+		for (int i = 0; i < size; i++)
+			for (int j = i; j < size; j++){
+				if(i == j) 
+					resp[i][j] = 0;
+				else if(x.getIth(i+j)){
+					resp[i][j] = 1;
+					resp[j][i] = 1;
+				}
+				else{
+					resp[i][j] = 0;
+					resp[j][i] = 0;
+				}
+			}
+		
+		return resp;
+	}
+	
+	private MultiGraph getGraph(double[][] m)
+	{
+		GraphViewer graphViewer = new GraphViewer();
+		
+		for (int i = 0; i < m.length; i++) {
+			graphViewer.addNode(i);
+		}
+		
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m.length; j++) {
+				if(m[i][j] == 1)
+					graphViewer.addEdge(i, j);
+			}
+		}
+		
+		return graphViewer.getGrafo();
+	}
+	
+	public void evaluateConstraints(Solution solution) throws JMException {
+
+
+	}
+
+}
